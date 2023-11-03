78ef21c48d202c7a9044ed197a15b3b8cc3f97e1
hartmann.pereira <hartmann.pereira@gmail.com>
2023-11-03 21:21:03 +0000

    Update streamlit_app.py
    just changing the variable name

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-second_fruit_choice = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', second_fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 
 
